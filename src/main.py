71ba8ba03fdb8e2f21fd9acddd54af62abfb861c
Devin Ford <devin.ford@selu.edu>
2024-02-20 21:25:33 -0600

    initial commit

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import discord
+from discord import Client, Intents, Message
+import responses
+
+TOKEN = 'MTIwOTYwMjc0ODkwODA0ODQ2NA.GmC8Ay.we4tIuF9OKsKTpgzrDoLSMsjU6nGzuAWBAMyQk'
+
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents = intents)
+
+async def send_message(message, user_message):
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response = responses.handle_response(message, user_message)
+        if response != None: 
+            await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is ready!')
+
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+        
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+
+    print(f"{username} said: {user_message} ({channel})")
+
+    await send_message(message, user_message)
+   
+
+def main():
+ client.run(token = TOKEN)
+
+if __name__ == "__main__":
+    main()
---
+++ src/responses.py
@@ -0,0 +1,37 @@
+import random
+import discord
+from discord import Message
+headsortails = ['Heads', 'Tails']
+
+def handle_response(message, user_message) -> str:
+
+    if user_message.startswith('!complain'):
+        user_message = user_message[10:]
+        member = discord.Member = message.author
+        return (f"{member.mention} nobody cares about '{user_message}', cry about it.")
+    
+    p_message = user_message.lower()
+
+    p_message = p_message.replace(" ","")
+
+    if p_message == "!help":
+        return "`[Help Message Here]`"
+    
+    if p_message == "!test":
+        return 'working'
+    
+    if p_message == "!flip":
+        return headsortails[random.randint(0,1)]
+    
+    if p_message == "!roll":
+        return str(random.randint(1,6))
+    
+    if p_message == "!rolld20": #make this d{any number} using similar method as !complain
+        return str(random.randint(1,20))
+    
+    if p_message == "🌞":
+        return "Praise the sun!"
+    
+    if "french" in p_message: #takes lots of power so maybe delete when not funny
+        return "Eww French"
+    
