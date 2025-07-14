a83b7ec5c5dbd71a337ae0818dd1dad4b5975975
Devin Ford <devin.ford@selu.edu>
2025-07-14 02:23:22 -0500

    GOD PLEASE WORK

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ headsortails = ["heads", "tails"]
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 solaire_quotes = [{"quote": "Praise the sun!", "author": "Solaire"}, {"quote": "If only I could be so grossly incandescent", "author": "Solaire"}, {"quote": "Oh, hello there. I will stay behind, to gaze at the sun. The sun is a wondrous body. Like a magnificent father", "author": "Solaire"}, {"quote": "You really are fond of chatting with me, aren't you? If I didn't know better, I'd think you had feelings for me! Ha ha ha", "author": "Solaire"}, {"quote": "I am Solaire of Astora, an adherent of the Lord of Sunlight. Now that I am Undead, I have come to this great land, the birthplace of Lord Gwyn, to seek my very own sun", "author": "Solaire"}, {"quote": "We are amidst strange beings, in a strange land. The flow of time itself is convoluted; with heroes centuries old phasing in and out", "author": "Solaire"}]
 stock_favorlvls = ['hated', 'poor', 'none', 'favored', 'loved']
-rarity_colors = {"Common": 0xf7faf8, "Uncommon": 0x14c744, "Rare": 0x1791e8, "Legendary": 0x7217e8, "Incandescent": 0xfcb632}
+rarity_colors = {"Common": 0xf7faf8, "Uncommon": 0x14c744, "Rare": 0x1791e8, "Legendary": 0x7217e8, "Incandescent": 0xfcb632, "Key-Item": 0x068067, "Monument": 0xfcf914}
 lock = asyncio.Lock()
 
 #events-----------------------------------------------------------------------------------------------
@@ -41,6 +41,7 @@ async def on_ready():
     general = await bot.fetch_channel(CID)
     stock_check.start()
     birthday_check.start()
+    shop_refresh.start()
     print(f"started tasks")
 
 @bot.event
@@ -64,7 +65,7 @@ async def birthday_check():
 
 @tasks.loop(time=datetime.time(hour=17, minute=0)) #1700 utc 1200 cdt
 async def stock_check():
-    message = f"Sunlight stock market report - {datetime.date.today()}\n"
+    message = f"@everyone\nSunlight stock market report - {datetime.date.today()}\n"
     with open('stocks.json') as f: # might need a lock
         stocks = json.load(f)
     if datetime.date.today().weekday() in (0, 4): # monday and thursday
@@ -91,6 +92,40 @@ async def stock_check():
     with open('stocks.json', 'w') as f: # might need a lock
         json.dump(stocks, f, indent=2)
     await general.send(message)
+
+@tasks.loop(time=datetime.time(hour=17, minute=30)) #1730 utc 1230 cdt
+async def shop_refresh():
+    with lock and open('items.json') as f:
+        items = json.load(f)
+    shop_items = {}
+    #Populate shop with Incandescent items
+    for _ in range(1):
+        choices = [item for item in items if items[item]['rarity'] == 'Incandescent']
+        chosen_one = random.choice(choices)
+        shop_items[chosen_one] = items[chosen_one]
+    #Populate shop with Legendary items
+    for _ in range(1):
+        choices = [item for item in items if items[item]['rarity'] == 'Legendary']
+        chosen_one = random.choice(choices)
+        shop_items[chosen_one] = items[chosen_one]
+    #Populate shop with Rare items
+    for _ in range(1):
+        choices = [item for item in items if items[item]['rarity'] == 'Rare']
+        chosen_one = random.choice(choices)
+        shop_items[chosen_one] = items[chosen_one]
+    #Populate shop with Uncommon items
+    for _ in range(1):
+        choices = [item for item in items if items[item]['rarity'] == 'Uncommon']
+        chosen_one = random.choice(choices)
+        shop_items[chosen_one] = items[chosen_one]
+        #Populate shop with Common items
+    for _ in range(1):
+        choices = [item for item in items if items[item]['rarity'] == 'Common']
+        chosen_one = random.choice(choices)
+        shop_items[chosen_one] = items[chosen_one]
+    with lock and open('shop.json', 'w') as f:
+        json.dump(shop_items, f, indent=2)
+    await general.send("@everyone\nShop has been refreshed.")
 
 #on message---------------------------------------------------------------------------------------------
 
@@ -291,6 +326,9 @@ async def stockname_autocomplete(interaction, current: str) -> typing.List[app_c
 @app_commands.autocomplete(stockname=stockname_autocomplete)
 async def buystock(interaction, stockname: str, amount: int = 1):
     """buys a stock"""
+    if amount < 0:
+        await interaction.response.send_message("NIGHTMARE NIGHTMARE NIGHTMARE NIGHTMARE NIGHTMARE", ephemeral=True)
+        return
     userid = str(interaction.user.id)
     with lock and open('stocks.json') as f:
         stocks = json.load(f)
@@ -321,11 +359,6 @@ async def buystock(interaction, stockname: str, amount: int = 1):
 async def sellstock(interaction, stockname: str, amount: str = "1"):
     """sells a stock"""
     userid = str(interaction.user.id)
-    with lock and open('stocks.json') as f:
-        stocks = json.load(f)
-    if stockname not in stocks:
-        await interaction.response.send_message("Invalid stock name", ephemeral=True)
-        return
     with lock and open('user-stocks.json') as f:
         investors = json.load(f)
     if stockname not in investors[userid]:
@@ -335,23 +368,28 @@ async def sellstock(interaction, stockname: str, amount: str = "1"):
         if investors[userid][stockname] <= int(amount):
             amount = investors[userid][stockname]
             del investors[userid][stockname]
+        elif int(amount) < 0:
+            await interaction.response.send_message("Don't go to sleep tonight", ephemeral=True)
+            return
         else:
-            amount = investors[userid][stockname]
             investors[userid][stockname] -= int(amount)
     except:
         if amount != "all":
             await interaction.response.send_message("Invalid amount", ephemeral=True)
             return
-        amount = investors[userid][stockname]
-        del investors[userid][stockname]
-    total = stocks[stockname]['price'] * amount
+        else:
+            amount = investors[userid][stockname]
+            del investors[userid][stockname]
+    with lock and open('stocks.json') as f:
+        stocks = json.load(f)
+    total = stocks[stockname]['price'] * int(amount)
     with lock and open('user-stocks.json', 'w') as f:
         json.dump(investors, f, indent=2)
     with lock and open('data.json') as f:
         data = json.load(f)
     data[userid]['sunlight'] = round(data[userid]['sunlight'] + total, 2)
-    with lock and open('user-stocks.json', 'w') as f:
-        json.dump(investors, f, indent=2)
+    with lock and open('data.json', 'w') as f:
+        json.dump(data, f, indent=2)
     await interaction.response.send_message(f"Sold {amount} shares of {stockname} for {total} Sunlight, Balance: {data[userid]['sunlight']} Sunlight", ephemeral=True)
 
 @bot.tree.command(guild=guild)
@@ -413,13 +451,135 @@ async def itemdesc(interaction, item: str):
     if item not in inventories[userid]:
         await interaction.response.send_message("You do not own any of this item")
     embed = discord.Embed(title=item, description=items[item]['description'], color=rarity_colors[items[item]['rarity']])
+    embed.add_field(name="Consumable", value=items[item]['consumable'], inline=True)
+    embed.add_field(name="Equipable", value=items[item]['equipable'], inline=True)
     embed.add_field(name="Price", value=f"{items[item]['price']} Sunlight", inline=True)
-    embed.add_field(name="Consumable", value=items[item]['consumable'])
     await interaction.response.send_message(embed=embed, ephemeral=True)
+
+@bot.tree.command(guild=guild)
+async def shop(interaction):
+    """shows shop"""
+    with lock and open('shop.json') as f:
+        shop_items = json.load(f)
+    embeds = []
+    for item in shop_items:
+        embed = discord.Embed(title=item, description=shop_items[item]['description'], color=rarity_colors[shop_items[item]['rarity']])
+        embed.add_field(name="Consumable", value=shop_items[item]['consumable'], inline=True)
+        embed.add_field(name="Equipable", value=shop_items[item]['equipable'], inline= True)
+        embed.add_field(name="Price", value=f"{shop_items[item]['price']} Sunlight", inline=True)
+        embeds.append(embed)
+    await interaction.response.send_message(embeds=embeds, ephemeral=True)
+
+async def shop_autocompletion(interaction, current: str) -> typing.List[app_commands.Choice[str]]:
+    with lock and open('shop.json') as f:
+        shop_items = json.load(f)
+    choices = []
+    for item in shop_items:
+        if current.lower() in item.lower():
+            choices.append(app_commands.Choice(name=item, value=item))
+    return choices
+
+@bot.tree.command(guild=guild)
+@app_commands.autocomplete(item=shop_autocompletion)
+async def buyitem(interaction, item: str, amount: int = 1):
+    """buys item"""
+    if amount < 0:
+        await interaction.response.send_message("You know what you did", ephemeral=True)
+        return
+    userid = str(interaction.user.id)
+    with lock and open('shop.json') as f:
+        shop_items = json.load(f)
+    if item not in shop_items:
+        await interaction.response.send_message("Item not in shop", ephemeral=True)
+        return
+    total = shop_items[item]['price'] * amount
+    with lock and open('data.json') as f:
+        data = json.load(f)
+    if data[userid]['sunlight'] < total:
+        await interaction.response.send_message(f"Not enought Sunlight, total: {total} Sunlight, balance: {data[userid]['sunlight']} Sunlight. broke mf", ephemeral=True)
+        return
+    data[userid]['sunlight'] = round(data[userid]['sunlight'] - total, 2)
+    with lock and open('data.json', 'w') as f:
+        json.dump(data, f, indent=2)
+    with lock and open('user-inventories.json') as f:
+        inventories = json.load(f)
+    if item not in inventories[userid]:
+        inventories[userid][item] = amount
+    else:
+        inventories[userid][item] += amount
+    with lock and open('user-inventories.json', 'w') as f:
+        json.dump(inventories, f, indent=2)
+    await interaction.response.send_message(f"Bought {amount} {item} for {total}, balance {data[userid]['sunlight']} Sunlight", ephemeral=True)
+
+@bot.tree.command(guild=guild)
+@app_commands.autocomplete(item=item_autocompletion)
+async def sellitem(interaction, item: str, amount: str = "1"):
+    """sells item"""
+    userid = str(interaction.user.id)
+    with lock and open('user-inventories.json') as f:
+        inventories = json.load(f)
+    if item not in inventories[userid]:
+        await interaction.response.send_message("Item not in inventory", ephemeral=True)
+        return
+    try:
+        if inventories[userid][item] <= int(amount):
+            amount = inventories[userid][item]
+            del inventories[userid][item]
+        elif int(amount) < 0:
+            await interaction.response.send_message("I will eat your kidneys", ephemeral=True)
+            return
+        else:
+            inventories[userid][item] -= int(amount)
+    except:
+        if amount != 'all':
+            await interaction.response.send_message("Invalid amount", ephemeral=True)
+            return
+        else:
+            amount = inventories[userid][item]
+            del inventories[userid][item]
+    with lock and open('user-inventories.json', 'w') as f:
+        json.dump(inventories, f, indent=2)
+    with lock and open('items.json') as f:
+        items = json.load(f)
+    total = items[item]['price'] * int(amount)
+    with lock and open('data.json') as f:
+        data = json.load(f)
+    data[userid]['sunlight'] = round(data[userid]['sunlight'] + total, 2)
+    with lock and open('data.json', 'w') as f:
+        json.dump(data, f, indent=2)
+    await interaction.response.send_message(f"Sold {amount} {item} for {total} Sunlight, balance {data[userid]['sunlight']} Sunlight", ephemeral=True)
+
+@bot.tree.command(guild=guild)
+async def resetshop(interaction):
+    """resets the shop"""
+    if interaction.user.id != ADMIN:
+        await interaction.response.send_message("Admin only")
+        return
+    await shop_refresh()
+    await interaction.response.send_message("Shop refreshed", ephemeral=True)
+
+@bot.tree.command(guild=guild)
+async def resetinventories(interaction):
+    """resets inventories"""
+    if interaction.user.id != ADMIN:
+        await interaction.response.send_message("Admin only")
+        return
+    inventories = {}
+    for user in interaction.guild.members:
+        inventories[user.id] = {}
+    with lock and open('user-inventories.json', 'w') as f:
+        json.dump(inventories, f, indent=2)
+    await interaction.response.send_message("Inventories reset")
+
+@bot.tree.command(guild=guild)
+async def testcolors(interaction):
+    """tests colors"""
+    channel = interaction.channel
+    for key in rarity_colors:
+        embed = discord.Embed(title=key, description=f"This color represents the {key} level of rarity", color=rarity_colors[key])
+        await channel.send(embed=embed)
+    await interaction.response.send_message("Rarity color embed test", ephemeral=True)
     
-# @bot.tree.command(guild=guild)
-# async def say(interaction):
-#     """mocks you"""
 
 def main():
     bot.run(TOKEN)
