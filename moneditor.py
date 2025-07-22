4f15c7c2e6e73a33276daf457be46eda30f8b234
Devin Ford <devin.ford@selu.edu>
2025-07-21 22:40:21 -0500

    skill tab done

--- moneditor.py
+++ moneditor.py
@@ -6,8 +6,6 @@ import re
 
 lock = asyncio.Lock()
 
-itemrarity_list = ['Common', 'Uncommon', 'Rare', 'Legendary', "Incandescent", 'Key-Item']
-
 with lock and open('gricklemon.json') as f:
     mons = json.load(f)
 with lock and open('skills.json') as f:
@@ -21,6 +19,8 @@ with lock and open('banners.json') as f:
 with lock and open('stocks.json') as f:
     stocks = json.load(f)
 
+skilllist = list(skills.keys())
+skilllist.append("new")
 itemlist = list(items.keys())
 itemlist.append("new")
 stocklist = list(stocks.keys())
@@ -60,14 +60,157 @@ def main():
 
     #skill editor---------------------------------------------------------------------------------------
 
+    skillscaling_list = ["S", "A", "B", "C", "D", "E", "-"]
+    skillstat_list = ['str', 'dex', 'int', 'fth', 'arc']
+    skillbanner_list = list(banners.keys())
+    skillbanner_list.remove('active')
+    skillbanner_list.remove('change-date')
+    skill_rarity = ['Common', 'Uncommon', 'Rare', 'Legendary', 'Incandescent']
+    status_effects = ['Poison', 'Paralysis']
+    skillstat_label_dict = {}
+    skillstat_option_dict = {}
+    skillstat_selected_dict={}
+    skillstatus_label_dict = {}
+    skillstatus_dict = {}
+    skillstatus_check_dict = {}
 
+    def skill_select_callback(skill):
+        if skill == "new":
+            skillname_entry.configure(textvariable=ctk.StringVar(master=stab, value=""))
+            selected_skillbanner.set("Normal")
+            selected_skillrarity.set("Common")
+            skillbasedmg_entry.configure(textvariable=ctk.StringVar(master=stab, value=""))
+            for stat in skillstat_list:
+                skillstat_selected_dict[stat].set("-")
+            for status in status_effects:
+                skillstatus_check_dict[status].deselect()
+            skilldescription_textbox.delete("0.0", "end")
+            skill_save_create.configure(text="Create Skill")
+            skill_save_create.configure(command=skill_create_callback)
+        else:
+            skillname_entry.configure(textvariable=ctk.StringVar(master=stab, value=skill))
+            selected_skillbanner.set(skills[skill]['banner'])
+            selected_skillrarity.set(skills[skill]['rarity'])
+            skillbasedmg_entry.configure(textvariable=ctk.StringVar(master=stab, value=str(skills[skill]['basedmg'])))
+            for stat in skillstat_list:
+                skillstat_selected_dict[stat].set(skills[skill]['scaling'][stat])
+            for status in status_effects:
+                if status in skills[skill]['statuses']:
+                    skillstatus_check_dict[status].select()
+            skilldescription_textbox.delete("0.0", 'end')
+            skilldescription_textbox.insert('0.0', skills[skill]['description'])
+            skill_save_create.configure(text="Save Changes")
+            skill_save_create.configure(command=skill_save_callback)
+
+    def skill_save_callback():
+        skill = skill_select.get()
+        new_skillname = skillname_entry.get()
+        new_skillbasedmg = skillbasedmg_entry.get()
+        new_skilldescription = skilldescription_textbox.get('0.0', 'end')
+        if "" in (new_skillname, new_skillbasedmg, new_skilldescription):
+            return
+        skills[new_skillname] = {'banner': skillbanner_select.get(), 'rarity': skillrarity_select.get(), 'basedmg': new_skillbasedmg, 'description': new_skilldescription, 'scaling': {stat: scale.get() for stat, scale in skillstat_option_dict.items()}, 'statuses': [status for status in status_effects if skillstatus_check_dict[status].get() == "True"]}
+        if skill != new_skillname:
+            del skills[skill]
+            skilllist.remove(skill)
+            skilllist.insert(0, new_skillname)
+            skill_select.configure(values=skilllist)
+            selected_skill.set(new_skillname)
+        with lock and open('skills.json', 'w') as f:
+            json.dump(skills, f, indent=2)
+
+    def skill_create_callback():
+        skillname = skillname_entry.get()
+        skillbasedmg = skillbasedmg_entry.get()
+        description = skilldescription_textbox.get('0.0', 'end')
+        if "" in (skillname, skillbasedmg, description) or skillname in skills:
+            return
+        skills[skillname] = {'banner': skillbanner_select.get(), 'rarity': skillrarity_select.get(), 'basedmg': skillbasedmg, 'description': description, 'scaling': {stat: scale.get() for stat, scale in skillstat_option_dict.items()}, 'statuses': [status for status in status_effects if skillstatus_check_dict[status].get() == "True"]}
+        with lock and open('skills.json', 'w') as f:
+            json.dump(skills, f, indent=2)
+        skilllist.insert(0, skillname)
+        skill_select.configure(values=skilllist)
+        selected_skill.set(skillname)
+        skill_select_callback(skillname)
+
+    selected_skill = ctk.StringVar(master=stab, value=skilllist[0])
+    skill_select = ctk.CTkOptionMenu(master=stab, values=skilllist, command=skill_select_callback, variable=selected_skill)
+    skill_select.grid(row=0, column=0, sticky='w')
+
+    skillname_label = ctk.CTkLabel(master=stab, text="Name:")
+    skillname_label.grid(row=1, column=0, sticky='w', pady=(10,0))
+
+    skillname_entry = ctk.CTkEntry(master=stab, textvariable=ctk.StringVar(master=stab, value=skill_select.get()))
+    skillname_entry.grid(row=2, column=0, sticky='w', pady=(10,0))
+
+    skillbanner_label = ctk.CTkLabel(master=stab, text="Banner:")
+    skillbanner_label.grid(row=3, column=0, sticky='w', pady=(10,0))
+
+    selected_skillbanner = ctk.StringVar(master=stab, value=skills[skill_select.get()]['banner'])
+    skillbanner_select = ctk.CTkOptionMenu(master=stab, values=skillbanner_list, variable=selected_skillbanner)
+    skillbanner_select.grid(row=4, column=0, sticky='w')
+
+    skillrarity_label = ctk.CTkLabel(master=stab, text="Rarity:")
+    skillrarity_label.grid(row=5, column=0, sticky='w', pady=(10,0))
+
+    selected_skillrarity = ctk.StringVar(master=stab, value=skills[skill_select.get()]['rarity'])
+    skillrarity_select = ctk.CTkOptionMenu(master=stab, values=skill_rarity, variable=selected_skillrarity)
+    skillrarity_select.grid(row=6, column=0, sticky='w')
+
+    skillbasedmg_label = ctk.CTkLabel(master=stab, text="Base Damage")
+    skillbasedmg_label.grid(row=7, column=0, sticky='w', pady=(10,0))
+
+    skillbasedmg_entry = ctk.CTkEntry(master=stab, validate="key", validatecommand=(valid_num, "%P"), textvariable=ctk.StringVar(master=stab, value=skills[skill_select.get()]['basedmg']))
+    skillbasedmg_entry.grid(row=8, column=0, sticky='w')
+
+    statrow = 9
+    for stat in skillstat_list:
+        skillstat_label_dict[stat] = ctk.CTkLabel(master=stab, text=f"{stat}:")
+        skillstat_label_dict[stat].grid(row=statrow, column=0, sticky='w', pady=(10,0))
+
+        skillstat_selected_dict[stat] = ctk.StringVar(master=stab, value=skills[skill_select.get()]['scaling'][stat])
+
+        skillstat_option_dict[stat] = ctk.CTkOptionMenu(master=stab, values=skillscaling_list, variable=skillstat_selected_dict[stat], width=20)
+        skillstat_option_dict[stat].grid(row=statrow, column=1, sticky='w', pady=(10,0))
+
+        statrow += 1
+
+    for status in status_effects:
+        skillstatus_label_dict[status] = ctk.CTkLabel(master=stab, text=f"{status}:")
+        skillstatus_label_dict[status].grid(row=statrow, column=0, sticky='w', pady=(10,0))
+
+        if stat in skills[skill_select.get()]['statuses']:
+            statbool = "True"
+        else:
+            statbool = "False"
+        skillstatus_dict[status] = ctk.StringVar(master=stab, value=statbool)
+
+        skillstatus_check_dict[status] = ctk.CTkCheckBox(master=stab, text="", variable=skillstatus_dict[status], onvalue="True", offvalue="False")
+        skillstatus_check_dict[status].grid(row=statrow, column=1, sticky='w', pady=(10,0))
+
+        statrow += 1
+
+    skilldescription_label = ctk.CTkLabel(master=stab, text="Description:")
+    skilldescription_label.grid(row=statrow, column=0, sticky='w', pady=(10,0))
+
+    statrow += 1
+    skilldescription = skills[skill_select.get()]['description']
+    skilldescription_textbox = ctk.CTkTextbox(master=stab)
+    skilldescription_textbox.insert("0.0", skilldescription)
+    skilldescription_textbox.grid(row=statrow, column=0, columnspan=2, sticky='ew')
+
+    statrow += 1
+    skill_save_create = ctk.CTkButton(master=stab, text="Save Changes", command=skill_save_callback)
+    skill_save_create.grid(row=statrow, column=0, sticky='w', pady=(10,0))
 
     #item editor------------------------------------------------------------------------------------------
+
+    itemrarity_list = ['Common', 'Uncommon', 'Rare', 'Legendary', "Incandescent", 'Key-Item']
 
     def item_select_callback(item):
         if item == "new":
             itemname_entry.configure(textvariable=ctk.StringVar(master=itab, value=""))
-            selected_rarity.set("Common")
+            selected_itemrarity.set("Common")
             itemprice_entry.configure(textvariable=ctk.StringVar(master=itab, value=""))
             itemconsumable_switch.deselect()
             itemstatuseffect_switch.deselect()
@@ -77,7 +220,7 @@ def main():
             item_save_or_create.configure(command=item_create_callback)
         else:
             itemname_entry.configure(textvariable=ctk.StringVar(master=itab, value=item))
-            selected_rarity.set(items[item]['rarity'])
+            selected_itemrarity.set(items[item]['rarity'])
             itemprice_entry.configure(textvariable=ctk.StringVar(master=itab, value=str(items[item]['price'])))
             itemconsumable.set(str(items[item]['consumable']))
             itemstatuseffect.set(str(items[item]['statuseffect']))
@@ -154,7 +297,6 @@ def main():
         item_select_callback(itemname)
         
     selected_item = ctk.StringVar(master=itab, value=itemlist[0])
-
     item_select = ctk.CTkOptionMenu(master=itab, values=itemlist, command=item_select_callback, variable=selected_item)
     item_select.grid(row=0, column=0, sticky='w')
     
@@ -164,11 +306,11 @@ def main():
     itemname_entry = ctk.CTkEntry(master=itab, textvariable=ctk.StringVar(master=itab, value=item_select.get()))
     itemname_entry.grid(row=2, column=0, sticky='w')
 
-    selected_rarity = ctk.StringVar(master=itab, value=items[item_select.get()]['rarity'])
+    selected_itemrarity = ctk.StringVar(master=itab, value=items[item_select.get()]['rarity'])
     itemrarity_label = ctk.CTkLabel(master=itab, text="Rarity:")
     itemrarity_label.grid(row=3, column=0, sticky='w', pady=(10,0))
 
-    itemrarity_select = ctk.CTkOptionMenu(master=itab, values=itemrarity_list, variable=selected_rarity)
+    itemrarity_select = ctk.CTkOptionMenu(master=itab, values=itemrarity_list, variable=selected_itemrarity)
     itemrarity_select.grid(row=4, column=0, sticky='w')
 
     itemprice_label = ctk.CTkLabel(master=itab, text="Price:")
@@ -261,7 +403,6 @@ def main():
         stock_select_callback(stockname)
 
     selected_stock = ctk.StringVar(value=stocklist[0])
-
     stock_select = ctk.CTkOptionMenu(master=stotab, values=stocklist, command=stock_select_callback, variable=selected_stock)
     stock_select.grid(row=0, column=0, sticky='w')
 
