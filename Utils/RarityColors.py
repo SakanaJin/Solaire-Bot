a82413ee040256e4d6a6fbb23150a5a6f45e64e7
Devin Ford <Devin.Ford4263@gmail.com>
2026-03-24 15:19:23 -0500

    rarity colors

---
+++ Utils/RarityColors.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class RarityColors(Enum):
+    "Common" = 0xf7faf8
+    "Uncommon" = 0x14c744
+    "Rare" = 0x1791e8
+    "Legendary" = 0x7217e8
+    "Incandescent" = 0xfcb632
+    "KeyItem" = 0x068067
+    "Monument" = 0xfcf914
